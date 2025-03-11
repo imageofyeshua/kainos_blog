d468ba65374de3ebee9fb19f28f665ea92537325
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-11 12:42:13 +0900

    Added Views Count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_11_005005) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_11_032848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_005005) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 end
