c214e48b2142012a04780fc653f3270120f981c4
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-13 16:05:30 +0900

    Navbar Align Fix

--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_142303) do
     t.index ["recipient_type", "recipient_id"], name: "index_noticed_notifications_on_recipient"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.string "recipient_type", null: false
+    t.bigint "recipient_id", null: false
+    t.string "type", null: false
+    t.jsonb "params"
+    t.datetime "read_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["read_at"], name: "index_notifications_on_read_at"
+    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
